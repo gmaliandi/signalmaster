8d9058a67f29ea12fb84e1e82e28ea8ab3ba19b0
Guido Leandro Maliandi <guido@unboundvr.com>
2015-10-22 12:37:55 -0300

    Deleting resources info when player leaves

--- sockets.js
+++ sockets.js
@@ -52,6 +52,8 @@ module.exports = function (server, config) {
                     client.room = undefined;
                 }
             }
+            
+            delete resources[client.id];
         }
 
         function join(name, cb) {
